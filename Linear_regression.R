30b14b0a7d4d7c80d84175674ecf9a0779841491
Jamilasullivan <sullivan-al-kadhomiyj@cardiff.ac.uk>
2026-04-14 01:00:13 +0100

    Initial notes and file set up.

---
+++ Linear_regression.R
@@ -0,0 +1,18 @@
+## Linear Regression ###########################################################
+
+# If x increases, how does y change?
+
+# Find the best straight line to predict a number
+
+
+# The model = y=β0​+β1​x
+
+# y = what you’re predicting
+# x = your input (feature)
+# β₀ = intercept (where the line starts)
+# β₁ = slope (how much y changes when x increases by 1)
+
+# error=(yactual​−ypredicted​)2 - (least squares for minimising error)
+
+# Pick β₀ and β₁ so the line hugs the data as tightly as possible
+
---
+++ Logistic_regression.R
@@ -0,0 +1,9 @@
+## Logistic Regression #########################################################
+
+# If x increases, how does the probability change
+
+# Changes linear regression outputs to possibilities
+
+# P(y=1)= 1/1+e(power of -z) - sigmoid function (converts any number into a probability)
+
+
